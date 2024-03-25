dc489d31a74d01795397838aee16b9fdee4f0947
yangfs <fushenyang@gmail.com>
2024-03-25 17:00:26 +0800

    完成的初始数据

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -23,11 +23,24 @@ export default function Index() {
 		<div>
 			<p className={`font-bold`}>资源信息（{content})</p>
 			{/* <button onClick={() => setCount(count + 1)}>点击 {count} 次</button> */}
-			<div className={`flex flex-col lg:flex-row lg:flex-wrap`}>
+			<div>
 				{fetcher.data !== undefined ? (
-					<div>
+					<div className={`flex flex-col lg:flex-row lg:flex-wrap`}>
 						{data?.map((item, index) => {
-							return <div key={index}>{item['url']}</div>;
+							return (
+								<div
+									key={index}
+									className={`m-1 border text-left lg:m-2 lg:basis-[calc(25%-1rem)] lg:bg-cyan-100 lg:pl-2`}
+								>
+									<span>项目名称：{item['program_name']}</span>
+									<span>项目类型：{item['program_type']}</span>
+									<span>
+										<a className="underline" href={item['url']}>
+											网址
+										</a>
+									</span>
+								</div>
+							);
 						})}
 					</div>
 				) : (

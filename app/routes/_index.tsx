43d675dee6b73de6481a7a649585a9ecb0842dff
ISNing <ISNing_SF@outlook.com>
2024-03-26 19:48:13 +0800

    New design for showing programs

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,8 @@
 import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { json, useFetcher, useLoaderData } from '@remix-run/react';
 import { useEffect } from 'react';
+import { ProgramCard } from '~/components';
+import { ProgramMetadata } from '~/types';
 // import { useState } from 'react';
 // import { Markdown } from '~/components';
 
@@ -25,21 +27,10 @@ export default function Index() {
 			{/* <button onClick={() => setCount(count + 1)}>点击 {count} 次</button> */}
 			<div>
 				{fetcher.data !== undefined ? (
-					<div className={`flex flex-col lg:flex-row lg:flex-wrap`}>
+					<div className={`flex flex-col space-y-8 lg:flex-row lg:flex-wrap`}>
 						{data?.map((item, index) => {
 							return (
-								<div
-									key={index}
-									className={`m-1 border text-left lg:m-2 lg:basis-[calc(25%-1rem)] lg:bg-cyan-100 lg:pl-2`}
-								>
-									<span>项目名称：{item['program_name']}</span>
-									<span>项目类型：{item['program_type']}</span>
-									<span>
-										<a className="underline" href={item['url']}>
-											网址
-										</a>
-									</span>
-								</div>
+								<ProgramCard program={new ProgramMetadata(item)} key={index} />
 							);
 						})}
 					</div>

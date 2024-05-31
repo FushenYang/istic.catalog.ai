3e0c2cfd98190a5266968236d6597afdcf5f7a27
Hexiang <qihexiang@outlook.com>
2024-05-31 15:32:25 +0800

    stroke-width to strokeWidth

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -51,7 +51,7 @@ export default function Index() {
 								cy="12"
 								r="10"
 								stroke="currentColor"
-								stroke-width="4"
+								strokeWidth="4"
 							></circle>
 							<path
 								className="opacity-75"
